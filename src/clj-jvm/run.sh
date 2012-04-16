89562cf305fb1e558ded6775942a12ba707514e0
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2012-04-16 13:40:00 -0700

    Update name where I like to keep latest ClojureDocs snapshot file on my local drive

--- src/clj-jvm/run.sh
+++ src/clj-jvm/run.sh
@@ -15,7 +15,7 @@ TOOLTIPS=no-tooltips
 #TOOLTIPS=tiptip
 
 CLOJUREDOCS_SNAPSHOT=""
-#CLOJUREDOCS_SNAPSHOT="${HOME}/clj/clojuredocs-snapshot-latest.txt"
+#CLOJUREDOCS_SNAPSHOT="${HOME}/clj/cd-snap.txt"
 
 # Optionally produce PDF files by running LaTeX.  See README.markdown
 # for notes on what parts of LaTeX are enough for this to work.
@@ -48,7 +48,7 @@ do
 	case "${CDOCS_SUMMARY}" in
 	no-cdocs-summary) CLOJUREDOCS_SNAPSHOT=""
 	                  ;;
-	cdocs-summary) CLOJUREDOCS_SNAPSHOT="${HOME}/clj/clojuredocs-snapshot-latest.txt"
+	cdocs-summary) CLOJUREDOCS_SNAPSHOT="${HOME}/clj/cd-snap.txt"
 	                  ;;
 	esac
 	TARGET="cheatsheet-${TOOLTIPS}-${CDOCS_SUMMARY}.html"
